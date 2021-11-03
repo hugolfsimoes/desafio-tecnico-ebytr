49be15019d91e5f14ebb21751a774cf83582bd6d
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-03 17:07:02 -0300

    criando pastas e arquvios  dos componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
-  )
-}
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2">
+
+        </Typography>
+      </AppBar>
+    </Container>
+  );
+};
 export default App;
