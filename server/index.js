ee569ca9367220cf9e550dad28fdf05eb9171ec1
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-03 16:14:56 -0300

    criado um middleware para gerenciar erros

--- server/index.js
+++ server/index.js
@@ -2,19 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const tasksRoutes = require('./routes/tasks.route.js');
+const error = require('./middlewares/error');
 
 const app = express();
 
-app.use('/tasks', tasksRoutes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-/* const CONNECTION_URL = 'mongodb+srv://hugolfsimoes:hugolfsimoes123@cluster0.d305m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; */
+app.use('/tasks', tasksRoutes);
+app.use(error);
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
-
-/* mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`)))
-  .catch((error) => console.log(error.message)); */
--- server/services/tasks.service.js
+++ server/services/tasks.service.js
@@ -2,4 +2,6 @@ const TasksModel = require('../models/tasks.model');
 
 const getAllTasks = async () => await TasksModel.getAllTasks();
 
+const createTask = async(payload);
+
 module.exports = { getAllTasks };
