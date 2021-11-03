821d7b102777ab633086a4cc4dcca17c92360516
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-03 17:58:36 -0300

    colocando um pouco de estilo nas paginas

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import logo from './images/logoEbytr.png';
 
 const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
-        <Typography variant="h2">
-
-        </Typography>
+        <Typography variant="h2" align="center">Tasks</Typography>
+        <img src={ logo } alt="Logo da empresa Ebytr" height="60" />
       </AppBar>
+      <Grow in >
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={ 3 }>
+            <Grid item xs={ 12 } sm={ 7 }>
+              <Tasks />
+            </Grid>
+            <Grid item xs={ 12 } sm={ 4 }>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
