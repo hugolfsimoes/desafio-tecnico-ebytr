6168e348f405ddf483c6485d9e2a36ab163f930b
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-03 18:48:02 -0300

    feito a forma de estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import './App.css';
 import Form from './components/Form';
 import Tasks from './components/Tasks';
 import logo from './images/logoEbytr.png';
-import useStyles from './styles';
 
 
 
 const App = () => {
-  const classes = useStyles();
   return (
-    <Container maxWidth="lg">
+    <div id="container">
+      <header id="header-tag">
+        <img id="logo-image" src={ logo } alt="Logo da empresa Ebytr" />
+        <h2 id="title-h2">Task Manager</h2>
+      </header>
+      <div id="content-container">
+        <aside id="form-field">
+          <Form />
+        </aside>
+        <main id="main-content">
+          <Tasks />
+        </main>
+      </div>
+    </div>
+    /* <Container maxWidth="lg">
       <AppBar className={ classes.appBar } position="static" color="inherit">
         <Typography className={ classes.heading } variant="h2" align="center">Tasks</Typography>
         <img className={ classes.image } src={ logo } alt="Logo da empresa Ebytr" height="100" />
@@ -27,7 +39,7 @@ const App = () => {
           </Grid>
         </Container>
       </Grow>
-    </Container>
+    </Container> */
   );
 };
 export default App;
