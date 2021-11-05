982c93594ecbba4d5b7b546bff98f8106bfc43a7
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-05 02:34:32 -0300

    trocando url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//const url = 'https://desafio-ebytr.herokuapp.com/tasks';
-const url = 'http://localhost:5000/tasks';
+const url = 'https://desafio-ebytr.herokuapp.com/tasks';
 
 export const fetchTasks = () => axios.get(url);
 export const createTask = (newTask) => axios.post(url, newTask);
